aea086ed6f52c0819cc3a4f372a21d3c21efe844
jproggar <jorgegarcia72@hotmail.com>
2021-12-18 06:17:07 -0500

    c

---
+++ src/pages/Index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Index = () => (
+  <div>
+    <div className='bg-gray-300 h-96'>.</div>
+    <div className='bg-gray-300 h-96'>.</div>
+    <div className='bg-gray-300 h-96'>.</div>
+    <div className='bg-gray-300 h-96'>.</div>
+  </div>
+);
+
+export default Index;
